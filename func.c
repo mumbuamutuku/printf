41e7a860d8d48d8a87d4771d397d498ea04c67bd
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-19 13:38:47 -0700

    func

--- func.c
+++ func.c
@@ -11,3 +11,91 @@ int print_char(va_list args)
 	c = va_arg(args, int);
 	return (_putchar(c));
 }
+/**
+* print_integer - prints integer
+* @args: integer argument
+* Return: the decimal function
+*/
+int print_integer(va_list args)
+{
+	return (print_d(args));
+}
+/**
+* print_string -priints striing
+* @args: string argument
+* Return: number of characters
+*/
+int print_string(va_list args)
+{
+	int i, count = 0;
+	char *str;
+
+	i = 0;
+	str = va_arg(args, char*);
+	if (str == NULL)
+		str = "(null)";
+
+	while (str[i] != '\0')
+
+	{
+
+		_putchar(str[i]);
+
+		i++;
+
+		count++;
+
+	}
+
+	return (count);
+}
+/**
+ * print_percent - pass the percent sing
+ * @args: string  argument
+ * Return: return the percent sing
+ */
+int print_percent(va_list args)
+{
+	char *str;
+
+	str = "%";
+	if (va_arg(args, int) == *str)
+	{
+		return (*str);
+	}
+	return (*str);
+}
+/**
+ * print_d - prints a decimal
+ * @args: decimal argument
+ * Return: counter
+ */
+int print_d(va_list args)
+{
+	unsigned int absolute, aux, countnum, count;
+	int n;
+
+	count = 0;
+	n = va_arg(args, int);
+	if (n < 0)
+	{
+		absolute = (n * -1);
+		count += _putchar('-');
+	}
+	else
+		absolute = n;
+
+	aux = absolute;
+	countnum = 1;
+	while (aux > 9)
+	{
+		aux /= 10;
+		countnum *= 10;
+	}
+	while (countnum >= 1)
+	{
+		count += _putchar(((absolute / countnum) % 10) + '0');
+		countnum /= 10;
+	}
+	return (count);
+}
