c04077aeed65a333e5095956d553cf0fe3e7e38d
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-19 12:25:22 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_percent(va_list args);
+int print_d(va_list args);
+int print_integer(va_list args);
+
+#endif
