69b7b72a5cb55cf0f366c0f3cbb6e315f2fa3228
Montanah <mititi.isaac@gmail.com>
2022-10-19 12:57:18 -0700

    main.h file edit

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
@@ -11,5 +12,6 @@ int print_str(va_list args);
 int print_percent(va_list args);
 int print_d(va_list args);
 int print_integer(va_list args);
+int parser(const char *format, conver_t f_list[], va_list arg_list);
 
 #endif
