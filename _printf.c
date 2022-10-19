ebb2bb5c9131b1540d026c422650b891ff612e88
Montanah <mititi.isaac@gmail.com>
2022-10-19 12:47:14 -0700

    printf.c commit

---
+++ _printf.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _printf - receives the main string and all the requisite parameters to
+ * print a formated string
+ * @format: a string containing all the desired characters
+ * Return: total count of the characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{NULL, NULL}
+	};
+	va_list arg;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(arg, format);
+
+	printed_chars = parser(format, f_list, arg);
+	va_end(arg);
+	return (printed_chars);
+}
