2f304cdcfa064b839b1cc4a0ffab161bbe72b229
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-21 02:35:00 -0700

    kok

--- _printf.c
+++ _printf.c
@@ -17,7 +17,7 @@ int _printf(const char *format, ...)
 		{"d", print_d},
 		{"i", print_integer},
 		{"b", printbinary},
-		{"u", print_unsigned_number},
+		{"u", print_unsigned_integer},
 		{NULL, NULL}
 	};
 	
