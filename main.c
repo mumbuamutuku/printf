d7b6373b8d880d7cde043419356117f330512220
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-21 12:05:05 -0700

    buffer

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ int main(void)
 {
  unsigned int ui;
  ui = (unsigned int)INT_MAX + 1024;
+_printf("There is %b bytes in %b KB\n", 1024, 1);
     _printf("%b\n", 98);
 _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 _printf("Unsigned:[%u]\n", ui);
