77ba5fe175264e772323d75dcd3eb073f91c25ac
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-21 07:14:45 -0700

    betty

--- _printf.c
+++ _printf.c
@@ -8,9 +8,7 @@
 int _printf(const char *format, ...)
 {
 	int printed_chars;
-	
 	conver_t f_list[] = {
-		
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_percent},
@@ -22,16 +20,12 @@ int _printf(const char *format, ...)
 		{"X", print_bighex},
 		{NULL, NULL}
 	};
-	
 	va_list args;
-	
+
 	if (format == NULL)
 		return (-1);
-	
 	va_start(args, format);
-	
 	printed_chars = parser(format, f_list, args);
-	
 	va_end(args);
 	return (printed_chars);
 }
